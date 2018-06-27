4eabed2995b79654b580c6ceedb1fb7181902888
James Tattersall <10601770+jamerst@users.noreply.github.com>
2018-06-27 16:08:10 +0100

    Fix styles, fix mobile styles, generally tidy, add final content

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
 $(document).ready(function () {
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 5
+        }, 500);
+    });
+
     $("body").on("click", ".close-button", function () {
         $(".cover").fadeOut(300, function () {
             $(".cover").remove();
         })
     });
 
-    $(".project-entry").click(function () {
-       $("body").append(`<div class="cover">
+    $(".project-entry").on("click", function () {
+        if (window.matchMedia("only screen and (max-device-width: 480px)").matches) {
+            $(location).attr("href", "./project_content/" + this.id + "/index.html");
+        } else {
+            $("body").append(`<div class="cover">
         <div class="page-content-center"></div>
         </div>`).hide().fadeIn(200);
 
-       $(".page-content-center").load("project_content/" + this.id + "/index.html .center-window", function () {
-           $(".page-content-center > .center-window > header").prepend("<div class='left'></div>").append("<div class='right'><div class='close-button' title='Close'><i class='far fa-times-circle'></i></div></div>");
-       })
+            $(".page-content-center").load("project_content/" + this.id + "/index.html .center-window", function () {
+                $(".page-content-center > .center-window > header").prepend("<div class='left'></div>").append("<div class='right'><div class='close-button' title='Close'><i class='far fa-times-circle'></i></div></div>");
+            });
+        }
    });
 });
